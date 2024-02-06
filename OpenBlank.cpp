add4383787085acb25db085975591c276d4cbf58
SonnyJim <sonny_jim_pin@yahoo.com>
2024-02-06 15:50:07 +1100

    Finish trimming openblank.cpp

--- OpenBlank.cpp
+++ OpenBlank.cpp
@@ -1,15 +1,9 @@
-//#include <SDL2/SDL.h>
-//#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
-#include "OpenBlank.h"
 #include "SDL.h"
 #include "Media.h"
 #include "Game.h"
-/*
-#include "Movement.h"
-#include "LTarget.h"
-*/
+
 bool quit = false;
 
 int main( int argc, char* args[] )
@@ -30,37 +24,8 @@ int main( int argc, char* args[] )
 		{	
 			while (!quit)
 				game.update();
-			/*
-			//Main loop flag
-
-			//Event handler
-			SDL_Event e;
-
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-					
-					//Handle button events
-					for( int i = 0; i < MAX_TARGETS; ++i )
-					{
-						gTargets[ i ].handleEvent( &e );
-					}
-				}
-				movement_all ();
-				sdl_render ();
-			}
-			*/
 		}
 	}
-
 	//Free resources and close SDL
 	sdl_close();
 
